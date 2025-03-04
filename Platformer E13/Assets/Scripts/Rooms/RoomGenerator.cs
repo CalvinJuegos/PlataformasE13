459340b0a9148855880b96c52a61ccc2b5427b79
guillemrs <79013333+guillemrs@users.noreply.github.com>
2025-03-04 19:07:15 +0100

    Progreso RoomGenerator

--- Platformer E13/Assets/Scripts/Rooms/RoomGenerator.cs
+++ Platformer E13/Assets/Scripts/Rooms/RoomGenerator.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
+    public GameObject player;
     public List<GameObject> StartingChunkPrefabs;
     public List<GameObject> ProceduralChunkPrefabs;
     public List<GameObject> EndingChunkPrefabs;
-    public Transform playerSpawnPoint;
     public int chunkLevel = 3; // Number of procedural chunks before the ending room
 
     void Start()
@@ -17,12 +17,17 @@ public class RoomGenerator : MonoBehaviour
 
     void GenerateRoom()
     {
+        // Implement random number for choosing elements from the list of prefabs
         // Instantiate the StartingChunk with the player at the specified spawn point
         GameObject startingChunk = Instantiate(StartingChunkPrefabs[0], transform.position, Quaternion.identity);
-        InstantiatePlayer(playerSpawnPoint.position);
+
+        // Get the player spawn point from the starting chunk
+        Transform playerSpawnPoint = startingChunk.transform.Find("PlayerSpawnPoint");
+        Instantiate(player, playerSpawnPoint.position, Quaternion.identity);
 
         // Generate procedural chunks based on the chunkLevel
         GameObject previousChunk = startingChunk;
+        // Instantiate the procedural chunks based on the previous chunk
         for (int i = 0; i < chunkLevel; i++)
         {
             foreach (Transform anchor in previousChunk.transform.Find("Anchors"))
@@ -31,6 +36,7 @@ public class RoomGenerator : MonoBehaviour
                 // FOR PROCEDURAL GENERARTION; A stored variable should be able to tell the next chunk if player is coming from the top or bottom of the previous chunk
                 // Instead of list of prefabs it should call a function that returns the prefab based on the previous chunk and the player's position
                 GameObject proceduralChunk = Instantiate(ProceduralChunkPrefabs[randomIndex], anchor.position, Quaternion.identity);
+                // Use GenerateChunk(anchor) function to generate the chunk
             }
         }
 
@@ -38,9 +44,9 @@ public class RoomGenerator : MonoBehaviour
         Instantiate(EndingChunkPrefabs[0], previousChunk.transform.Find("Anchors").GetChild(0).position, Quaternion.identity);
     }
 
-    void InstantiatePlayer(Vector3 position)
+    void GenerateChunk(Vector3 position)
     {
-        // Instantiate the player at the specified position
-        // You can instantiate your player prefab here
+        // Instantiate the procedural chunks based on the previous chunk
+
     }
 }
